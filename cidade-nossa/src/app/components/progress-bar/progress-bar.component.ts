da1cf44ee2416a6c2d5ba42fc0b49e74626c3da2
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-19 16:14:51 -0300

    create component de progresso

---
+++ cidade-nossa/src/app/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit, OnChanges {
+
+  @Input() public totalProblemasCriados!: number;
+  @Input() public totalProblemasResolvidos!: number;
+  progresso!: string;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getProgress();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.getProgress()
+;  }
+
+  getProgress(){
+    console.log(this.totalProblemasCriados)
+    this.progresso = ((this.totalProblemasResolvidos / this.totalProblemasCriados)*100).toString();
+  }
+
+}
