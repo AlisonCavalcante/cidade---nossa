81cee390ff12784ee7e0c06460acf58b7284bfbd
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-19 11:19:57 -0300

    create component search

---
+++ cidade-nossa/src/app/components/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  textSearch = ""
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  search(){
+    console.log(this.textSearch)
+    this.emmitSearch.emit(this.textSearch);
+  }
+}
