659b5eb0189f37dd4c2fc5c3d540ed0cea2f2bc1
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-20 00:23:34 -0300

    add atributos na interface Poster

--- cidade-nossa/src/app/shared/models/Poster.ts
+++ cidade-nossa/src/app/shared/models/Poster.ts
@@ -1,7 +1,13 @@
+import { IComentario } from './Comentario';
+
 export interface IPoster{
   id?: number;
   titulo: string;
   descricao: string;
+  dataCriacao: string;
   hashtags: string;
   likes: number;
+  comentarios?: IComentario[];
+  isComment?: boolean;
+  isAberto?: boolean;
 }
