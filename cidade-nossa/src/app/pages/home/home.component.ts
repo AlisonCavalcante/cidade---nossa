7b8302248a0bda163811d386bcd64fc5682718bc
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-20 14:39:46 -0300

    add comentários

--- cidade-nossa/src/app/pages/home/home.component.ts
+++ cidade-nossa/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { IComentario } from './../../shared/models/Comentario';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IComentario, IComentarioEnvio } from './../../shared/models/Comentario';
 import { ComentariosService } from './../../services/comentarios.service';
 import { IPoster } from './../../shared/models/Poster';
 import { PosterService } from './../../services/poster.service';
@@ -8,38 +9,114 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   listPosters!: IPoster[];
-  totalPostersCriados!: number;
+  setlistPosters!: IPoster[];
+  totalPostersCriados: number = 0;
+  totalPostersResolvidos: number = 0;
   comentarios!: IComentario[];
+  comentarios3!: IComentario[];
+  coment!: IComentarioEnvio;
   subscription!: Subscription;
-  constructor(private posterService: PosterService, private comentariosService: ComentariosService) { }
+  formComentario!: FormGroup;
+
+  constructor(
+    private posterService: PosterService,
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-   this.subscription =  this.posterService.getPosters().subscribe(res =>{
-       this.listPosters = res;
-       this.totalPostersCriados = this.listPosters.length;
-       console.log(this.totalPostersCriados)
-       console.log(this.listPosters);
-     })
-     this.comentariosService.getAll().subscribe(res => {
-      this.comentarios = res;
-      console.log(this.comentarios)
-     });
+    this.comentarios3 = [];
+    this.initiFormComentario();
+    this.subscription = this.posterService.getPosters().subscribe((res) => {
+      this.listPosters = res;
+      this.setlistPosters = res;
+      for (let i = 0; i < this.listPosters.length; i++) {
+        this.listPosters[i].comentarios = []
+        if (this.listPosters[i].isAberto == false) {
+          this.totalPostersResolvidos += 1;
+        }
+      }
+      this.comentariosService.getAll().subscribe((res) => {
+        this.comentarios = res;
+        this.organizarComentarios();
+      });
+    });
+
+    this.getTotalPosters();
   }
 
-  ngOnDestroy(): void {
-
+  getTotalPosters() {
+    this.posterService.getTotalPosters().subscribe((res) => {
+      this.totalPostersCriados = res;
+    });
   }
 
-  comentar(index: number){
+  ngOnDestroy(): void {}
 
+  initiFormComentario() {
+    this.formComentario = this.formBuilder.group({
+      comentario: [null, Validators.required],
+    });
   }
-  like(poster: IPoster){
+
+  habilitarComentar(poster: IPoster, index: number) {
+    this.listPosters[index].isComment = true;
+  }
+
+  // Método para formatar a data
+  /*formatarData(data: string[]): string {
+    let dia = data[2].slice(0, 2);
+    let mes = data[1];
+    let ano = data[0];
+    let dataFormatada = `${dia} -${mes} - ${ano}`;
+    return dataFormatada;
+  }
+*/
+  comentar(poster: IPoster, index: number) {
+    this.listPosters[index].isComment = false;
+    this.coment = this.formComentario.value;
+    this.coment.poster = poster;
+    // delete this.coment.poster.dataCriacao; //deleta uma chave de um objeto, somente se ele tiver a possibilidade de ser undefined
+    console.log(this.coment)
+    this.comentariosService.create(this.coment).subscribe((res) => {
+      this.resetForm();
+        if (this.listPosters[index].comentarios) {
+          this.listPosters[index].comentarios?.push(res);
+        }
+    });
+  }
+
+  like(poster: IPoster) {
     poster.likes += 1;
-    this.posterService.updatePoster(poster).subscribe(res => console.log(res));
+    this.posterService
+      .updatePoster(poster)
+      .subscribe((res) => console.log(res));
+  }
+
+  organizarComentarios() {
+    for (let i = 0; i < this.listPosters.length; i++) {
+      if (this.comentarios.length != 0) {
+        for (let j = 0; j < this.comentarios.length; j++) {
+          if (this.listPosters[i].id == this.comentarios[j].poster.id) {
+            this.listPosters[i].comentarios?.push(this.comentarios[j]);
+          }
+        }
+      }
+    }
+  }
+
+  resetForm() {
+    this.formComentario.reset();
+  }
+
+  searchProblemas(event: string) {
+    const produtosFilter = this.setlistPosters.filter((poster: IPoster) => {
+      return !poster.titulo.indexOf(event);
+    });
+    this.listPosters = produtosFilter;
   }
 }
