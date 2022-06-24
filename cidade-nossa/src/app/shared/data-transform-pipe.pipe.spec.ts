25175109cd7464c6fff6a843c3d99309cf70a983
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-24 10:04:16 -0300

    create data pipe

---
+++ cidade-nossa/src/app/shared/data-transform-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DataTransformPipePipe } from './data-transform-pipe.pipe';
+
+describe('DataTransformPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DataTransformPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
