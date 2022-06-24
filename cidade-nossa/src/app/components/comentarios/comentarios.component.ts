0e4cbc0f8d975c4e0b47e38553126532216a28fb
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-24 10:06:57 -0300

    insert button delete comentário

--- cidade-nossa/src/app/components/comentarios/comentarios.component.ts
+++ cidade-nossa/src/app/components/comentarios/comentarios.component.ts
@@ -1,26 +1,41 @@
 import { IComentario } from './../../shared/models/Comentario';
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { ComentariosService } from 'src/app/services/comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.css']
+  styleUrls: ['./comentarios.component.css'],
 })
 export class ComentariosComponent implements OnInit, OnChanges {
-
   @Input() comentarios!: IComentario[] | undefined;
   isVisible: boolean = true;
-  constructor() { }
+  constructor(private comentarioService: ComentariosService) {}
 
   ngOnInit(): void {
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-      console.log(changes)
-  }
-  ocultar(){
-    this.isVisible = !this.isVisible;
+
   }
 
-  favoritarComentario(){}
+  ocultar() {
+    this.isVisible = !this.isVisible;
+
+  }
+  delete(comentario: IComentario, index: number) {
+    this.comentarioService.delete(comentario.id).subscribe(res => {
+      this.comentarios?.splice(index,1)
+    });
+
+  }
+
+  favoritarComentario() {}
 }
